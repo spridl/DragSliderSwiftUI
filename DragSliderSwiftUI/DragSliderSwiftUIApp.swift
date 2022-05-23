305629ff691721aab32e5eee26cb99db5e7e4842
Tim <spridl@mail.ru>
2022-05-23 18:02:51 +0300

    Initial Commit

---
+++ DragSliderSwiftUI/DragSliderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragSliderSwiftUIApp.swift
+//  DragSliderSwiftUI
+//
+//  Created by Тимур on 23.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct DragSliderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
